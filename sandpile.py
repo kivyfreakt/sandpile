b24549915409f2b8b6183d58b781d8bc7afb198f
musicfreakt <yalovega.nikita5@gmail.com>
2019-07-03 05:24:36 +0500

    some updates

--- sandpile.py
+++ sandpile.py
@@ -3,30 +3,30 @@
 # Functions and class for sandpiles.
 
 class Sandpile():
-    def __init__(self, width, height, max_s = 3):
+    def __init__(self, width, height, max_sand = 3):
         super(Sandpile, self).__init__()
         self.width = width
         self.height = height
-        self.max_sand = max_s + 1
+        self.max_grains = max_sand + 1
         self.sandpile = [[0 for y in range(self.height)] for x in range(self.width)]
 
     def check_topple(self):
         num = []
         for x in range(self.width):
             for y in range(self.height):
-                if self.sandpile[x][y] >= self.max_sand:
+                if self.sandpile[x][y] >= self.max_grains:
                     num.append([x,y])
         if len(num) > 0:
             return num
         else:
             return None
 
-    def topple_batch(self, num):
+    def topple(self, num):
         newpile = self.sandpile
         for pos in num:
             x = pos[0]
             y = pos[1]
-            newpile[x][y] = self.sandpile[x][y] - self.max_sand
+            newpile[x][y] = self.sandpile[x][y] - self.max_grains
             if x+1 < self.width:
                 newpile[x+1][y]+=1
             if x-1 >= 0:
@@ -38,10 +38,10 @@ class Sandpile():
 
         self.sandpile = newpile
 
-    def topple(self):
+    def run(self):
         num = self.check_topple()
         while num != None:
-            self.topple_batch(num)
+            self.topple(num)
             num = self.check_topple()
 
     def get_pile(self):
@@ -51,8 +51,11 @@ class Sandpile():
         self.sandpile[x][y] = number
 
 if __name__ == '__main__':
-    pile = Sandpile(3,3)
-    pile.set_sand(20, 1, 1)
-    print(pile.get_pile())
-    pile.topple()
-    print(pile.get_pile())
+    pile = Sandpile(9,9)
+    pile.set_sand(1000, 4, 4)
+    pile.run()
+    grid = pile.get_pile()
+    for x in range(9):
+        for y in range(9):
+            print(grid[x][y], end = ' ')
+        print()
