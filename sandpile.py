5811fc41b79b5a586fb54a774c75221400d60c0b
musicfreakt <yalovega.nikita5@gmail.com>
2019-07-03 06:26:08 +0500

    use numpy arrays

--- sandpile.py
+++ sandpile.py
@@ -2,60 +2,42 @@
 # 03-07-2019 by musicfreakt
 # Functions and class for sandpiles.
 
+import numpy as np
+
 class Sandpile():
     def __init__(self, width, height, max_sand = 3):
         super(Sandpile, self).__init__()
         self.width = width
         self.height = height
         self.max_grains = max_sand + 1
-        self.sandpile = [[0 for y in range(self.height)] for x in range(self.width)]
+        self.grid = np.zeros((width,height), int)
 
-    def check_topple(self):
-        num = []
-        for x in range(self.width):
-            for y in range(self.height):
-                if self.sandpile[x][y] >= self.max_grains:
-                    num.append([x,y])
-        if len(num) > 0:
-            return num
-        else:
-            return None
+    def topple(self, max_sand):
+        self.grid[max_sand] -= self.max_grains
+        self.grid[1:,:][max_sand[:-1,:]] += 1
+        self.grid[:-1,:][max_sand[1:,:]] += 1
+        self.grid[:,1:][max_sand[:,:-1]] += 1
+        self.grid[:,:-1][max_sand[:,1:]] += 1
 
-    def topple(self, num):
-        newpile = self.sandpile
-        for pos in num:
-            x = pos[0]
-            y = pos[1]
-            newpile[x][y] = self.sandpile[x][y] - self.max_grains
-            if x+1 < self.width:
-                newpile[x+1][y]+=1
-            if x-1 >= 0:
-                newpile[x-1][y]+=1
-            if y+1 < self.height:
-                newpile[x][y+1]+=1
-            if y-1 >= 0:
-                newpile[x][y-1]+=1
-
-        self.sandpile = newpile
 
     def run(self):
-        num = self.check_topple()
-        while num != None:
-            self.topple(num)
-            num = self.check_topple()
+        while np.max(self.grid) >= self.max_grains:
+            max_sand = self.grid >= self.max_grains
+            self.topple(max_sand)
 
     def get_pile(self):
-        return self.sandpile
+        return self.grid
 
     def set_sand(self, number, x, y):
-        self.sandpile[x][y] = number
+        self.grid[x][y] = number
+
 
 if __name__ == '__main__':
-    pile = Sandpile(9,9)
-    pile.set_sand(1000, 4, 4)
+    pile = Sandpile(11,11)
+    pile.set_sand(4096, 5, 5)
     pile.run()
     grid = pile.get_pile()
-    for x in range(9):
-        for y in range(9):
+    for x in range(11):
+        for y in range(11):
             print(grid[x][y], end = ' ')
         print()
