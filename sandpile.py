9b6777967611a4053db6cbcf61e6bf10efd9a1a3
musicfreakt <yalovega.nikita5@gmail.com>
2019-07-05 00:32:05 +0500

    add visualisation with matplotlib

--- sandpile.py
+++ sandpile.py
@@ -3,14 +3,26 @@
 # Functions and class for sandpiles.
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Sandpile():
-    def __init__(self, width = 3, height = 3, max_sand = 3):
-        super(Sandpile, self).__init__()
-        self.width = width
-        self.height = height
-        self.max_grains = max_sand + 1
-        self.grid = np.zeros((width,height), int)
+    def __init__(self, arr = None ,rows = 3, cols = 3, max_sand = 4):
+        """
+        arr - 2d array of values
+        rows - height of sandpile
+        cols - width of sandpile
+        max_sand - max count of sandpile grains (> 3)
+        """
+        if arr == None:
+            self.rows = rows
+            self.cols = cols
+            self.max_grains = max_sand
+            self.grid = np.zeros((cols,rows), int)
+        else:
+            self.rows = len(arr)
+            self.cols = len(arr[0])
+            self.max_grains = max_sand
+            self.grid = np.array(arr)
 
     def topple(self, max_sand):
         self.grid[max_sand] -= self.max_grains
@@ -30,13 +42,14 @@ class Sandpile():
     def set_sand(self, x, y, number):
         self.grid[x][y] = number
 
+    def show(self):
+        plt.matshow(self.grid, cmap=plt.get_cmap('gist_rainbow'))
+        plt.axis('off')
+        filename = "sandpile.png"
+        plt.savefig(filename, bbox_inches='tight')
 
 if __name__ == '__main__':
-    pile = Sandpile(37,37)
-    pile.set_sand(18, 18, 65536)
+    pile = Sandpile(rows = 101, cols = 101)
+    pile.set_sand(50, 50, 65536)
     pile.run()
-    grid = pile.get_pile()
-    for x in range(37):
-        for y in range(37):
-            print(grid[x][y], end = ' ')
-        print()
+    pile.show()
